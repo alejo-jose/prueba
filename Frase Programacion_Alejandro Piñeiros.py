5538e347683f2e21135716becff52a520a665d52
alejo-jose <67164878+alejo-jose@users.noreply.github.com>
2020-06-19 13:10:53 -0500

    Add files via upload

---
+++ Frase Programacion_Alejandro Piñeiros.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 12:25:26 2020
+
+@author: Alejandro
+"""
+nombre= input("Ingrese su Nombre: ")
+ape= input("Ingrese su Apellido: ")
+ubi= input("Ingrese su Ubicacion: ")
+edad= input("Ingrese su Edad: ")
+edad1=str(edad)
+espacio=" "
+print("Hola "+nombre +espacio +ape +espacio +"estar en" +espacio +ubi +espacio +"es fantastico por que es un lugar maravilloso"+espacio +"y tener" +espacio +edad1 +espacio +"años" +espacio +"no te impide estar y conocer distintas ubicaciones.") 
